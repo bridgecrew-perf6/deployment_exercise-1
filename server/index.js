3587f5374b9101193c48c285691a642c1d2adbcd
Nicholas Anderson <anderson.nick@me.com>
2022-04-06 11:40:42 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const exp = require('constants')
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.get('/', (req,res)=> {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+const PORT = process.env.PORT || 4005
+
+app.listen(PORT, ()=> {console.log(`Listening on ${PORT}`)})
